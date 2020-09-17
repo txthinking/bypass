d5bf6817c244a651fdc0f438787772d0b8f0c95f
txthinking <cloud@txthinking.com>
2020-09-17 23:21:26 +0800

    update

--- china/cidr.sh
+++ china/cidr.sh
@@ -5,3 +5,4 @@
 
 curl -s https://raw.githubusercontent.com/gaoyifan/china-operator-ip/ip-lists/china.txt > ../chinacidr4.list
 curl -s https://raw.githubusercontent.com/gaoyifan/china-operator-ip/ip-lists/china6.txt > ../chinacidr6.list
+echo '17.0.0.0/8' >> ../chinacidr4.list
