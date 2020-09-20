8bfe89ab037978d9d269c08e252d51ff0f809214
txthinking <cloud@txthinking.com>
2020-09-20 21:10:07 +0800

    add C4

--- china/cidr.sh
+++ china/cidr.sh
@@ -3,6 +3,13 @@
 
 # TODO we may add lan CIDR
 
-curl -s https://raw.githubusercontent.com/gaoyifan/china-operator-ip/ip-lists/china.txt > ../chinacidr4.list
-curl -s https://raw.githubusercontent.com/gaoyifan/china-operator-ip/ip-lists/china6.txt > ../chinacidr6.list
+curl -x socks5://127.0.0.1:1080 https://raw.githubusercontent.com/gaoyifan/china-operator-ip/ip-lists/china.txt > ../chinacidr4.list
+curl -x socks5://127.0.0.1:1080 https://raw.githubusercontent.com/gaoyifan/china-operator-ip/ip-lists/china6.txt > ../chinacidr6.list
 echo '17.0.0.0/8' >> ../chinacidr4.list
+echo '0.0.0.0/8' >> ../chinacidr4.list
+echo '10.0.0.0/8' >> ../chinacidr4.list
+echo '127.0.0.0/8' >> ../chinacidr4.list
+echo '169.254.0.0/16' >> ../chinacidr4.list
+echo '172.16.0.0/12' >> ../chinacidr4.list
+echo '192.168.0.0/16' >> ../chinacidr4.list
+echo '224.0.0.0/4' >> ../chinacidr4.list
