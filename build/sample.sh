945c2c51944ec112d614a33aa44a51a681885acf
TxThinking <cloud@txthinking.com>
2021-03-29 23:00:15 +0800

    Create sample.sh

---
+++ build/sample.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+cat \
+  ../apple/domain.txt \
+  ../douban/domain.txt \
+  | sort | uniq > ../sample_domain.txt
+  
+cat \
+  ../apple/cidr4.txt \
+  ../internal/cidr4.txt \
+  | sort | uniq > ../sample_cidr4.txt
+
+cat \
+  ../apple/cidr6.txt \
+  | sort | uniq > ../sample_cidr6.txt
+ 
+cat \
+  ../ads/baidu.txt \
+  ../ads/taobao.txt \
+  | sort | uniq > ../sample_ad.txt
