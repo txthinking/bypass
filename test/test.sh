3b25d2f6f99dc337bda27d45c16435749f331a05
TxThinking <cloud@txthinking.com>
2023-10-29 10:09:37 -0500

    Update test.sh

--- test/test.sh
+++ test/test.sh
@@ -1,4 +1,9 @@
 #!/usr/bin/env bash
 
-mkdir out
-echo hello > out/hello
+curl https://bash.ooo/nami.sh | bash
+export PATH=$HOME/.nami/bin:$PATH
+
+nami install 7z
+nami install bun
+nami install jb
+nami install zigup
